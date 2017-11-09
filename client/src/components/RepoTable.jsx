0fe4d02b8e4c1232c5adafdcbb09aa1fce1fd707
Daniel Roberts <BeardOfDan@gmail.com>
2017-11-09 11:17:09 -0800

    Render the repo's owner's avatar and the repos' description
    If the description is over a set number of characters long, then
    it will be reduced to the limited size with an elipse (...)

--- client/src/components/RepoTable.jsx
+++ client/src/components/RepoTable.jsx
@@ -6,12 +6,29 @@ class RepoTable extends React.Component {
   }
 
   render() {
+    // styles
+    // const tdStyle = { marginRight: 10 };
+    const imgStyle = { maxHeight: 50, maxWidth: 50 };
+    const aStyle = { textDecoration: 'none' };
+
+    // helper function
+    const limitSize = function (str) {
+      const maxSize = 100;
+      if (str.length > (maxSize - 3)) {
+        return str.slice(0, 47) + '...';
+      } else {
+        return str;
+      }
+    }
+
     return (<table>
       <thead>
         <tr>
+          <th>Avatar</th>
           <th>Owner Login</th>
           <th>Repo Name</th>
           <th>Fork Count</th>
+          <th>Description</th>
         </tr>
       </thead>
       <tbody>
@@ -20,9 +37,11 @@ class RepoTable extends React.Component {
           // might have to use spread operator to get the elements to render
           this.props.repos.map((repo, index, array) => {
             return (<tr key={index}>
-              <td><a href={`https://github.com/${repo.owner.login}`}>{repo.owner.login}</a></td>
-              <td><a href={`https://github.com/${repo.full_name}`}>{repo.name}</a></td>
+              <td><img src={`${repo.owner.avatar_url}`} style={imgStyle} alt={`Avatar for ${repo.owner.login}`} /></td>
+              <td><a href={`https://github.com/${repo.owner.login}`} style={aStyle}>{repo.owner.login}</a></td>
+              <td><a href={`https://github.com/${repo.full_name}`} style={aStyle}>{repo.name}</a></td>
               <td>{repo.forks}</td>
+              <td>{limitSize(repo.description)}</td>
             </tr>);
           })
         }
